83a6a455d59e863e0f6f6c54801e234b04317602
wisnia01 <wasniewski.jakub@wp.pl>
2022-04-18 15:59:00 +0200

    KNN

---
+++ linear_regression_test.py
@@ -0,0 +1,12 @@
+
+
+# class LinearRegression:
+#
+#     def __init__(self, lr=0.001, n_iters=1000):
+#         self.lr = lr
+#         self.n_iters = n_iters
+#         self.weights = None
+#         self.bias = None
+#
+#     def fit:
+#
