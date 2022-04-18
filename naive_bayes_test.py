621bf6a7cbab080b22c687e9d74c5ada0c59ca5b
wisnia01 <wasniewski.jakub@wp.pl>
2022-04-18 21:51:13 +0200

    naive bayes

---
+++ naive_bayes_test.py
@@ -0,0 +1,21 @@
+import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+
+def accuracy(y_true, y_pred):
+    accuracy = np.sum(y_true == y_pred) / len(y_true)
+    return accuracy
+
+
+X, y = datasets.make_classification(n_samples=1000, n_features=10, n_classes=2, random_state=2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
+
+from naive_bayes import NaiveBayes
+
+nb = NaiveBayes()
+nb.fit(X_train, y_train)
+predictions = nb.predict(X_test)
+
+print(accuracy(y_test, predictions))
